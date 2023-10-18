ba3bbff1169f76cd5ab60d51594bbd7ac2a9e3d9
JIAJUN-YAO <66053749+JIAJUN-YAO@users.noreply.github.com>
2023-10-18 17:42:34 +1100

    Information upload

---
+++ Information.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import  Clienttemplates  from "../components/Clienttemplate/Clienttemplate";
+
+
+function Information(){
+    return(
+        
+        <div>
+            <Clienttemplates/>
+            <Clienttemplates/>
+
+
+            
+        </div>
+        
+
+         
+    )
+}
+export default Information;
