d2b0b2fb9eeda190cf6ab4ae2f0d0fe95d69c2f8
JIAJUN-YAO <66053749+JIAJUN-YAO@users.noreply.github.com>
2023-10-18 17:38:53 +1100

    Add files via upload
    Test upload

---
+++ Settings.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div>
+      <header className="Header">
+        <h1>SETTING</h1>
+      </header>
+
+      <main>
+        <div>
+          <h1 id="meHeading">My Settings</h1>
+
+          <button type="button" onClick={() => {}}>
+            See all profiles
+          </button>
+
+          <button type="button" onClick={() => {}}>
+            Privacy and Accessibility
+          </button>
+
+          <button type="button" onClick={() => {}}>
+            Help and Support
+          </button>
+
+          <button type="button" onClick={() => {}}>
+            Feedback
+          </button>
+
+          <button type="button" onClick={() => {}}>
+            Log out
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Settings;
